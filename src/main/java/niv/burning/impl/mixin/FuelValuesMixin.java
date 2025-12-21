546c60d2703a1461268647f1d73a6b7d78357d5d
Niv Oridocs <fradige95@gmail.com>
2025-12-21 13:33:34 +0100

    Fix  burning context base implementations

---
+++ src/main/java/niv/burning/impl/mixin/FuelValuesMixin.java
@@ -0,0 +1,37 @@
+package niv.burning.impl.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.FuelValues;
+import niv.burning.api.BurningContext;
+
+@Mixin(FuelValues.class)
+class FuelValuesMixin implements BurningContext {
+
+    @Unique
+    @Override
+    public boolean isFuel(Item item) {
+        return ((FuelValues) (Object) this).isFuel(new ItemStack(item));
+    }
+
+    @Unique
+    @Override
+    public boolean isFuel(ItemStack itemStack) {
+        return ((FuelValues) (Object) this).isFuel(itemStack);
+    }
+
+    @Unique
+    @Override
+    public int burnDuration(Item item) {
+        return ((FuelValues) (Object) this).burnDuration(new ItemStack(item));
+    }
+
+    @Unique
+    @Override
+    public int burnDuration(ItemStack itemStack) {
+        return ((FuelValues) (Object) this).burnDuration(itemStack);
+    }
+}
