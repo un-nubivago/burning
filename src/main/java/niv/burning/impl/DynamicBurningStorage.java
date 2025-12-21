384d3ae744cdcef7691a14e717097cb08607fc5b
Niv Oridocs <fradige95@gmail.com>
2025-12-21 13:08:36 +0100

    Fix dynamic burning storage visibility test issue

--- src/main/java/niv/burning/impl/DynamicBurningStorage.java
+++ src/main/java/niv/burning/impl/DynamicBurningStorage.java
@@ -1,5 +1,7 @@
 package niv.burning.impl;
 
+import org.jetbrains.annotations.ApiStatus.Internal;
+
 import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
 import net.fabricmc.fabric.api.transfer.v1.transaction.base.SnapshotParticipant;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -8,7 +10,8 @@ import niv.burning.api.BurningContext;
 import niv.burning.api.BurningStorage;
 import niv.burning.api.BurningStorageHelper;
 
-class DynamicBurningStorage
+@Internal
+public class DynamicBurningStorage
         extends SnapshotParticipant<DynamicBurningStorage.Snapshot>
         implements BurningStorage {
 
